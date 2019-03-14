a645f2e45e6c8b535b52ae820dd84fba40353e16
Jeff Li <jli40@stuy.edu>
2019-03-14 04:07:52 -0400

    with comments

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,14 +1,16 @@
 public class List_inArraySlots{
 
 	private Object[] elements;
-	private int filledElements;
+	private int filledElements; //Number of elements in list
 
 	private static final int INITIAL_CAPACITY = 10;
 	
+	//Construct list with small initial capacity
 	public List_inArraySlots() {
 		elements = new Object[INITIAL_CAPACITY];
 	}
 	
+	//@return string representation of list in [a, b, c, ] format
 	public String toString() {
 		String s = "[";
 		for(Object obj : elements) {
@@ -17,10 +19,13 @@ public class List_inArraySlots{
 		return s + "]";
 	}
 	
+	//@return the number of elements in the list
 	public int size() {
 		return filledElements;
 	}
 	
+	//adds a value to the end of the list
+	//@return true
 	public boolean add(Object obj){
 		if (elements.length == filledElements) expand();
 		elements[filledElements] = obj;
@@ -40,6 +45,7 @@ public class List_inArraySlots{
 		return add(new Boolean(b));
 	}
 
+	//Doubles the capacity of the list
 	public void expand() {
 		Object[] dummyArr = new Object[elements.length * 2];
 		for(int index = 0; index < filledElements; index++) {
@@ -48,6 +54,7 @@ public class List_inArraySlots{
 		elements = dummyArr;
 	}
 	
+	//@return element of specified index of the list
 	public Object get(int index) {
 		return elements[index];
 	}
